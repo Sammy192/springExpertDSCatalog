8f80776b6186b1705bcacd75c8144401973cf12c
Samuel R Borges <sammy_rb@hotmail.com>
2024-02-03 07:34:58 -0300

    Created findCategoryById, exception handler

--- backend/src/main/java/com/devsamuca/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsamuca/dscatalog/services/CategoryService.java
@@ -3,12 +3,14 @@ package com.devsamuca.dscatalog.services;
 import com.devsamuca.dscatalog.dto.CategoryDTO;
 import com.devsamuca.dscatalog.entities.Category;
 import com.devsamuca.dscatalog.repositories.CategoryRepository;
+import com.devsamuca.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,5 +31,13 @@ public class CategoryService {
             listDto.add(new CategoryDTO(cat));
         }
         return listDto;*/
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+
+        return new CategoryDTO(entity);
     }
 }
