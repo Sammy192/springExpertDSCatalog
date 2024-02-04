7b4f35ebb9e7a86a06a6909eacfe15cbdf924428
Samuel R Borges <sammy_rb@hotmail.com>
2024-02-03 22:48:44 -0300

    created auditing data

--- backend/src/main/java/com/devsamuca/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsamuca/dscatalog/entities/Category.java
@@ -2,14 +2,21 @@ package com.devsamuca.dscatalog.entities;
 
 import jakarta.persistence.*;
 
+import java.time.Instant;
+
 @Entity
 @Table(name = "tb_category")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
     public Category() {
     }
 
@@ -32,6 +39,24 @@ public class Category {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
