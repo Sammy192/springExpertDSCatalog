47aa7c597bc2421d07cc0610a3f0b3a386d37916
Samuel R Borges <sammy_rb@hotmail.com>
2024-03-29 10:24:45 -0300

    created emailDTO to recover password

---
+++ backend/src/main/java/com/devsamuca/dscatalog/resources/AuthResource.java
@@ -0,0 +1,33 @@
+package com.devsamuca.dscatalog.resources;
+
+import com.devsamuca.dscatalog.dto.EmailDTO;
+import com.devsamuca.dscatalog.dto.UserDTO;
+import com.devsamuca.dscatalog.dto.UserInsertDTO;
+import com.devsamuca.dscatalog.dto.UserUpdateDTO;
+import com.devsamuca.dscatalog.services.AuthService;
+import com.devsamuca.dscatalog.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(value = "/recover-token")
+    public ResponseEntity<Void> createRecoverToken(@Valid @RequestBody EmailDTO body) {
+        authService.createRecoverToken(body);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ backend/src/main/java/com/devsamuca/dscatalog/services/AuthService.java
@@ -0,0 +1,8 @@
+package com.devsamuca.dscatalog.services;
+
+import com.devsamuca.dscatalog.dto.EmailDTO;
+
+public class AuthService {
+    public void createRecoverToken(EmailDTO body) {
+    }
+}
